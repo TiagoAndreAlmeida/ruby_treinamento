65f9def7e6491f2ed06942a9bd4cc618cb24ffd8
Tiago <tiago@rubert.com.br>
2018-11-17 15:04:45 -0300

    ruby treinamento

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,6 @@
+//= require jquery
+//= require jquery_ujs
+//= require activestorage
+//= require turbolinks
+//= require bootstrap
+//= require adminlte
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController
+    def main
+        @msg = "Hello World"
+    end
+
+end
